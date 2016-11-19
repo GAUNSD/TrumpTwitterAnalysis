9a43d7d3687c785158a44ad02904636e41848e7f
Gustavo Murcia <gmurcia@uwo.ca>
2016-11-19 18:23:35 -0500

    Now getting NYT in order of newest to oldest from eelection day backwards

--- NYT Scraper/nyTimesScrape.py
+++ NYT Scraper/nyTimesScrape.py
@@ -4,29 +4,15 @@ import time
 
 api = articleAPI('ec4da91764da4217983fadb8c85f1dca')
 
-articles = api.search(q = "Trump",
-		fq = {'headline':'Trump'},
-		begin_date = '20160707',
-		end_date = '20161107',
-		fl = 'headline,pub_date,word_count',
-		sort='oldest',
-		page = '101')
-
-
-print articles
-
-print "----------------------------------------"
-
-
 all_articles = []
-for j in range(0,100): #NYT limits pager to first 100 pages. But rarely will you find over 100 pages of results anyway.
+for j in range(0,101): #NYT limits pager to first 100 pages. But rarely will you find over 100 pages of results anyway.
 	time.sleep(6) #API calls limited to 5 seconds. make it 6 so it doesn't get angry
 	articles = api.search(q = "Trump",
 		fq = {'headline':'Trump'},
 		begin_date = '20160707',
 		end_date = '20161107',
 		fl = 'headline,pub_date,word_count',
-		sort='oldest',
+		sort='newest',
 		page = str(j))
 
 	print j
